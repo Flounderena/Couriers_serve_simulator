5405b9a86d0ff31e2b54ce6b2ff94a572071d93a
Lena <ekrivchanskaya@gmail.com>
2025-10-21 23:44:37 +0300

    Create corier_service.cpp

---
+++ corier_service/corier_service.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include "Dispatcher.h"
+#include <random>
+#include <chrono>
+
+
+int main()
+{
+    int num_of_offices, num_of_couriers, step; std::cin >> num_of_offices >> num_of_couriers >> step;
+
+    Dispatcher d(num_of_offices, num_of_couriers, step);
+
+    d.programm_work();
+
+    return 0;
+
+}
+
+
+
+
