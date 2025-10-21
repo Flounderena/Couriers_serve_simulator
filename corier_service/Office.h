7b9343ab0cd5efcbd4bcdf29d6640d7e6a308874
Lena <ekrivchanskaya@gmail.com>
2025-10-22 01:07:26 +0300

    Update Office.h

--- corier_service/Office.h
+++ corier_service/Office.h
@@ -7,21 +7,12 @@
 class Office
 {
 public:
-	Office(int a, int b): cord_x(a), cord_y(b){}
+	Office(int a, int b);
 	~Office() {}
 
-	double get_distance(Office other) {
-		double dist_x = (cord_x - other.cord_x) * (cord_x - other.cord_x);
-		double dist_y = (cord_y - other.cord_y) * (cord_y - other.cord_y);
-		double dist = dist_x + dist_y;
-		double ans = sqrt(dist);
-		return ans;
-	}
+	double get_distance(Office other);
 
-	std::pair<int, int> get_pos() {
-		return std::make_pair(cord_x, cord_y);
-	}
-
+	std::pair<int, int> get_pos();
 
 private:
 	int cord_x, cord_y;
