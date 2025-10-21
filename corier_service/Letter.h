54b61a17bad2efbec77f4d447e16aea58768babe
Lena <ekrivchanskaya@gmail.com>
2025-10-21 23:45:01 +0300

    Create Letter.h

---
+++ corier_service/Letter.h
@@ -0,0 +1,58 @@
+#pragma once
+class Letter
+{
+public:
+	
+	Letter(int f, int s, int t, int l_t, int l_cnt) : from_of(f), to_of(s), order_time(t), deadline(l_t), delivered(false), t_begin(0), t_end(0), num_of_letter(l_cnt) {}
+	~Letter() {}
+
+	void set_t_begin(int t) {
+		t_begin = t;
+	}
+
+	void set_t_end(int t) {
+		t_end = t;
+	}
+
+	int get_t_begin() {
+		return t_begin;
+	}
+
+	int get_t_end() {
+		return t_end;
+	}
+
+	int get_beg_of() {
+		return from_of;
+	}
+
+	int get_end_of() {
+		return to_of;
+	}
+
+	int get_order_time() {
+		return order_time;
+	}
+
+	int get_deadline() {
+		return deadline;
+	}
+
+	int get_letter_num() {
+		return num_of_letter;
+	}
+
+	bool is_delievered() {
+		return delivered;
+	}
+
+	bool get_delivered() {
+		delivered = true;
+	}
+
+private:
+	int from_of, to_of, order_time, deadline, num_of_letter;
+	bool delivered;
+	int t_begin, t_end;
+};
+
