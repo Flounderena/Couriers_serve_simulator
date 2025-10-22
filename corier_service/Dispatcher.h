bca8dedd2d2c7dc0135039ee9ca3cb1fa4517637
Lena <ekrivchanskaya@gmail.com>
2025-10-22 06:57:43 +0300

    Update Dispatcher.h

--- corier_service/Dispatcher.h
+++ corier_service/Dispatcher.h
@@ -8,30 +8,34 @@
 class Dispatcher
 {
 public:
-    Dispatcher (int off, int cour, int s);
+    Dispatcher (int off, int cour, int s); // конструктор по 3 аргументам: кол-во филлиалов, кол-во курьеров, величина шага
+    // в конструкторе создаётся массив филлиалов с координтатами (у меня координаты не больше 30,т.к. время перемещения = расстянию между филлиалами)
+    // надо поменять 30 на размеры окна и в матрице путей делить расстояние на скорость курьера (желательно, чтобы в матрице ways не было чисел больше 20)
     ~Dispatcher() {}
 
-    std::queue<Letter> get_delivered_letters();
+    std::queue<Letter> get_delivered_letters(); // возвращает очередь доставленных писем
 
-    std::queue<Letter> get_ordered_letters();
+    std::queue<Letter> get_ordered_letters(); // возвращает очередь закаазанных, но не доставленных писем
 
-    std::queue<Letter> get_waiting_letters();
+    std::queue<Letter> get_waiting_letters(); // возвращает очередь писем, ждущих, пока за ними приедет курьер из другого филлиала (тоже недоставленные)
 
-    int get_distance(int a, int b);
+    int get_distance(int a, int b); // возвращает расстояние между филлиалами а и b 
 
-    Office get_ofice(int n);
+    Office get_ofice(int n); // возвращает n тый филлиал 
 
-    Courier get_courier(int n);
+    Courier get_courier(int n); // возвращает n того курьера
 
-    int get_num_free_rides();
+    int get_num_free_rides(); // возвращает количество холостых поездок
 
-    int get_sum_of_deviations();
+    int get_sum_of_deviations(); // возвращает суммарное время опозданий
 
-    void lett_deliver(Courier& cour, int t);
+    int get_free_rides_time(); // возвращает общее время холостых поездок
 
-    void equal_begin_time(Courier& cour);
+    void lett_deliver(Courier& cour, int t); // отмечает доставленные этим курьером к моменту времени t письма
 
-    void programm_work();
+    void equal_begin_time(Courier& cour);  // пересчитывает время доставки для каждого письма
+
+    void programm_work(); // весь функционал программы
 
 private:
 	std::queue<Letter> letters;
@@ -40,6 +44,6 @@ private:
 	std::vector<Courier> couriers;
 	std::vector<Office> offices;
 	std::vector<std::vector<int>> ways;
-	int num_free_rides, sum_of_deviations, letters_cnt;
+	int num_free_rides, sum_of_deviations, letters_cnt, free_rides_time;
 	int num_of_offices, num_of_couriers, step;
 };
