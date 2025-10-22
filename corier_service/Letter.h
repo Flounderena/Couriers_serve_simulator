6f609184503cdf8792c2b46705edbc417e0d9bd7
Lena <ekrivchanskaya@gmail.com>
2025-10-22 06:57:29 +0300

    Update Letter.h

--- corier_service/Letter.h
+++ corier_service/Letter.h
@@ -3,30 +3,28 @@ class Letter
 {
 public:
 	
-	Letter(int f, int s, int t, int l_t, int l_cnt);
+	Letter(int f, int s, int t, int l_t, int l_cnt); // конструктор по 5 параметрам: филлиал заказа, целевой филлиал, время заказа, дедлайн, порядковый номер
 	~Letter() {}
 
-	void set_t_begin(int t);
+	void set_t_begin(int t); // устанавливает время взятия письма курьером
 
-	void set_t_end(int t);
+	void set_t_end(int t); // устанавливает время доставки письма
 
-	int get_t_begin();
+	void set_beg_of(int of); // устанавливает филлиал, из которого данное письмо везётся
 
-	int get_t_end();
+	int get_t_begin(); // возвращает время взятия письма курьером
 
-	int get_beg_of();
+	int get_t_end(); // возвращает время доставки письма
 
-	int get_end_of();
+	int get_beg_of(); // возвращает филлиал, в котором его заказали
 
-	int get_order_time();
+	int get_end_of(); // возвращает филлиал, в который надо отвезти
 
-	int get_deadline();
+	int get_order_time(); // возвращает время заказа
 
-	int get_letter_num();
+	int get_deadline(); // возвращает дэдлайн заказа
 
-	bool is_delievered();
-
-	void get_delivered();
+	int get_letter_num(); // возвращает порядковый номер письма (который по счёту из всех)
 
 private:
 	int from_of, to_of, order_time, deadline, num_of_letter;
